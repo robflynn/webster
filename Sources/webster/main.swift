7c6840f1a490ab9e321f1706e14f0126f80d1778
Rob Flynn <rob@thingerly.com>
2018-11-28 00:42:48 -0500

    Add logger

--- Sources/webster/main.swift
+++ Sources/webster/main.swift
@@ -1,4 +1,8 @@
 import Foundation
+import SwiftyBeaver
+
+let logger = SwiftyBeaver.self
+logger.addDestination(ConsoleDestination())
 
 print("hi. i'm webster. /\\oo/\\")
 
@@ -18,20 +22,16 @@ func fetchHTML(from url: URL, completion: ((String) -> Void)?) {
     request.setValue("text/html; charset=utf-8", forHTTPHeaderField: "Accept")
     request.setValue("text/html; charset=utf-8", forHTTPHeaderField: "Content-Type") 
 
-    print("About to build it")
-
     let task = URLSession.shared.dataTask(with: request) { data, response, error in
             guard
                 let httpResponse = response as? HTTPURLResponse,
                 (200..<300) ~= httpResponse.statusCode,
                 let data = data else {
-                    print("well the data was wonk")
                     assertionFailure("Some kind of network request problem happened!")
 
                     return
                 }
                 guard let html = String(data: data, encoding: .utf8) else {
-                    print("well, decoding it was wonk")
                     assertionFailure("We failed on converting the html DATA to STRING.")
 
                     return
@@ -40,7 +40,9 @@ func fetchHTML(from url: URL, completion: ((String) -> Void)?) {
                 print("oh, okay, cool, lets callback")
                 completion?(html)
     }
-    print("about to fire it up! fire it up! fire it up!")
+
+    logger.debug("Fetching url....")
+
     task.resume()
 }
 
